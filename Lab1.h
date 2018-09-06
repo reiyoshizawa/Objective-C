583d5693fbb16282e5472fb84b0f167cd2f42086
yoshizawarei <yoshizawarei@mba.hitronhub.home>
2018-09-06 08:55:37 -0700

    first commit

---
+++ Lab1.h
@@ -0,0 +1,13 @@
+//
+//  Lab1.h
+//  Lab2
+//
+//  Created by yoshizawarei on 2018/09/05.
+//  Copyright © 2018年 yoshizawarei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lab1 : NSObject
+
+@end
